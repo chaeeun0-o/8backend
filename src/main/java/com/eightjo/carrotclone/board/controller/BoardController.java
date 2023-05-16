12d06bb3c73a30255cd6ddc549ab5b725b8be42d
rnjswo9578 <rnjswo153486@gmail.com>
2023-05-16 22:48:03 +0900

    fix: ResponseEntity wildcard

--- src/main/java/com/eightjo/carrotclone/board/controller/BoardController.java
+++ src/main/java/com/eightjo/carrotclone/board/controller/BoardController.java
@@ -39,7 +39,7 @@ public class BoardController {
     }
 
     @GetMapping("/board")
-    public ResponseEntity<DefaultDataRes<String>> getPosts(Pageable pageable, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails ) {
+    public ResponseEntity<DefaultDataRes<PageDto>> getPosts(Pageable pageable, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails ) {
         PageDto pageDto = boardService.getAllPost(pageable, userDetails);
         return ResponseEntity.ok(new DefaultDataRes<>(ResponseMessage.BOARD_GET, pageDto));
     }
